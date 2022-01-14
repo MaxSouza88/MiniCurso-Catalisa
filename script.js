799c56b0401bd2057b8b6706e2186f8527da8d9e
MaxSouza88 <souza1988@hotmail.com>
2022-01-14 17:28:23 -0300

    alterando comandos

--- script.js
+++ script.js
@@ -3,3 +3,20 @@ const botao = document.querySelector('button');
 const nomeDoPersonagem = document.querySelector('#nome');
 const nomeDoPersonagem = document.querySelector('#especie');
 const nomeDoPersonagem = document.querySelector('#status');
+
+pegarPersonagem = () => {
+return fetch(`https://rickandmortyapi.com/api/character/2`, {
+    method:'GET',
+    headers: {
+        Accept: 'application/json',
+        "Content-type": 'application/json'
+    }
+}).then((response) => response.json()).then((data) => {
+    imagem.src = data.image;
+    imagem.alt = data.name;
+});
+
+
+}
+
+botao.onclick = pegarPersonagem;
